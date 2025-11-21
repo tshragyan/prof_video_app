12682396482e541614c1af3b04e66c2c82c4bd1f
Daniel <dchragyan1@gmail.com>
2025-11-21 13:33:54 +0400

    db seed

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ShopifyErrorLog;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +10,14 @@ class DashboardController extends Controller
 {
     public function home(Request $request)
     {
+        ShopifyErrorLog::query()->create(
+            [
+                'user_id' => 1,
+                'method' => 'main',
+                'data' => $request->all(),
+            ]
+        );
+
         return Inertia::render('Home', [
             'message' => 'Welcome Daniel!'
         ]);

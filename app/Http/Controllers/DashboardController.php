0e99e58fd93243c29d5b768f8dc17f31f31eeb3d
Daniel <dchragyan1@gmail.com>
2025-11-25 23:43:48 +0400

    setting up app bridge

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,9 +10,23 @@ class DashboardController extends Controller
 {
     public function home(Request $request)
     {
-        return Inertia::render('Home', [
-            'message' => 'Welcome Daniel!',
-            'shop' => $request->get('shop')
-        ]);
+        try {
+
+            return Inertia::render('Home', [
+                'message' => 'Welcome Daniel!',
+                'shop' => $request->get('shop')
+            ]);
+        } catch (\Throwable $e) {
+
+
+            ShopifyErrorLog::query()->create(
+                [
+                    'user_id' => 1,
+                    'method' => 'dashboard',
+                    'data' => $e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine(),
+
+                ]
+            );
+        }
     }
 }

d52cc4cef261e5ef521b9a359898d0cb42c3a9f8
Daniel <dchragyan1@gmail.com>
2025-11-30 14:20:59 +0400

    setting app,navigation with polaris web components

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,31 @@
 <template>
-    <s-app-nav>
-        <s-link href="/" rel="home">Home</s-link>
-        <s-link href="/templates">Templates</s-link>
-        <s-link href="/settings">Settings</s-link>
-    </s-app-nav>
+    <s-app-provider
+        api-key="{{ config('shopify.api_key') }}"
+        host="{{ request('host') }}"
+    >
+        <s-app-nav>
+            <s-app-nav-item
+                label="Dashboard"
+                icon="HomeIcon"
+                href="/"
+            ></s-app-nav-item>
+
+            <s-app-nav-item
+                label="Products"
+                icon="Package2Icon"
+                href="/products"
+            ></s-app-nav-item>
+
+            <s-app-nav-item
+                label="Settings"
+                icon="SettingsIcon"
+                href="/settings"
+            ></s-app-nav-item>
+        </s-app-nav>
+
+        <div id="app"><!-- your Vue/Inertia app mounts here --></div>
+    </s-app-provider>
+
     <s-page heading="Videocrat">
             <s-text>Shoppable Videos</s-text>
         <s-grid

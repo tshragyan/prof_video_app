8750fb309439d90a95313b5332cc66fad67666bf
Daniel <dchragyan1@gmail.com>
2025-11-30 14:23:58 +0400

    setting app,navigation with polaris web components

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,43 +1,6 @@
 <template>
-    <s-app-provider
-        api-key="{{ config('shopify.api_key') }}"
-        host="{{ request('host') }}"
-    >
-        <s-app-nav>
-            <s-app-nav-item
-                label="Dashboard"
-                icon="HomeIcon"
-                href="/"
-            ></s-app-nav-item>
 
-            <s-app-nav-item
-                label="Products"
-                icon="Package2Icon"
-                href="/products"
-            ></s-app-nav-item>
 
-            <s-app-nav-item
-                label="Settings"
-                icon="SettingsIcon"
-                href="/settings"
-            ></s-app-nav-item>
-        </s-app-nav>
-
-        <div id="app"><!-- your Vue/Inertia app mounts here --></div>
-    </s-app-provider>
-
-    <s-page heading="Videocrat">
-            <s-text>Shoppable Videos</s-text>
-        <s-grid
-            gridTemplateColumns="repeat(2, 1fr)"
-            gap="small"
-            justifyContent="center"
-        >
-            <s-grid-item gridColumn="span 1" border="base" borderStyle="dashed">
-                Imported Videos
-            </s-grid-item>
-        </s-grid>
-    </s-page>
 
 </template>
 
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,32 @@
     @vite('resources/css/app.css')
 </head>
 <body>
- @inertia
+<s-app-provider
+    api-key="{{ config('shopify.api_key') }}"
+    host="{{ request('host') }}"
+>
+    <s-app-nav>
+        <s-app-nav-item
+            label="Dashboard"
+            icon="HomeIcon"
+            href="/"
+        ></s-app-nav-item>
+
+        <s-app-nav-item
+            label="Products"
+            icon="Package2Icon"
+            href="/products"
+        ></s-app-nav-item>
+
+        <s-app-nav-item
+            label="Settings"
+            icon="SettingsIcon"
+            href="/settings"
+        ></s-app-nav-item>
+    </s-app-nav>
+
+    @inertia
+
+</s-app-provider>
 </body>
 </html>

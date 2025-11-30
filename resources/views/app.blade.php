8435c61e2717ef94ad66afef6b997747f3e4501e
Daniel <dchragyan1@gmail.com>
2025-11-30 14:25:27 +0400

    setting app,navigation with polaris web components

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <!-- Polaris CSS -->
     <script src="https://cdn.shopify.com/shopifycloud/polaris.js"></script>
     <script type="module" src="https://cdn.shopify.com/storefront/web-components.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/@shopify/polaris@9.0.0/build/esm/styles.css">
@@ -9,6 +8,8 @@
     @vite('resources/css/app.css')
 </head>
 <body>
+{{config('shopify.api_key')}}
+{{request('host')}}
 <s-app-provider
     api-key="{{ config('shopify.api_key') }}"
     host="{{ request('host') }}"

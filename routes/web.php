8f8a2b310c834a4166b89e4d81783ad43b257a0d
Daniel <dchragyan1@gmail.com>
2025-12-04 16:39:44 +0400

    setting app,navigation with polaris web components

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('auth/install', [AuthController::class, 'install'])->name('shopify.install');
 Route::middleware('shopify.host')->group(function() {
     Route::get('/', [DashboardController::class, 'home'])->name('dashboard.home');
     Route::prefix('auth')->name('shopify.')->group(function() {
         Route::get('callback', [AuthController::class, 'callback'])->name('callback');
-        Route::get('install', [AuthController::class, 'install'])->name('.install');
     });
 });
 

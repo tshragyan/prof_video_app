7f95cae16496a55f5b67b6e539cdd0a014561157
Daniel <dchragyan1@gmail.com>
2026-01-13 22:07:03 +0400

    import videos directly from instagram

--- resources/js/Components/VideoUploader.vue
+++ resources/js/Components/VideoUploader.vue
@@ -7,15 +7,37 @@
             multiple
             @input="uploadFile"
             border="none"
+            v-show="!loading"
         />
         <s-divider/>
 
-        <video src=""></video>
+        <video
+            src=""
+            v-show="!loading"
+        ></video>
 
-        <s-stack direction="inline" gap="base">
-            <s-box v-for="(video, index) in videos" :key="index">
+        <s-stack
+            direction="inline"
+            gap="base"
+            v-show="!loading"
+        >
+            <s-box
+                v-for="(video, index) in videos"
+                :key="index"
+                v-show="!loading"
+            >
                 <video :src="video.src" :data-id="video.id" controls width="200"></video>
             </s-box>
+        </s-stack>
+
+        <s-stack
+            alignItems="center"
+            gap="base"
+            padding="large"
+            v-show="loading"
+        >
+            <s-spinner accessibilityLabel="Loading products" size="large" />
+            <s-text>Importing video...</s-text>
         </s-stack>
 
         <s-button slot="secondary-actions" commandFor="video-uploader-modal" command="--hide">
@@ -40,7 +62,7 @@ import {initShopifyAppBridge} from "../shopify";
 import {getSessionToken} from '@shopify/app-bridge-utils';
 
 let videos = ref([])
-
+const loading = ref(false)
 async function uploadFile(e) {
     const video = e.target.files[0]
     const form = new FormData();
@@ -66,15 +88,27 @@ async function saveVideos() {
 
     let app = initShopifyAppBridge();
     let token = await getSessionToken(app);
+    let response
 
-    const response = await axios.post(`${import.meta.env.VITE_APP_URL}/api/video/upload`, form, {
-        headers: {
-            "Content-Type": "multipart/form-data",
-            Authorization: `Bearer ${token}`,
-        },
-        onUploadProgress: (progressEvent) => {
-        },
-    });
+    try {
+        response = await axios.post(`${import.meta.env.VITE_APP_URL}/api/vid4eo/upload`, form, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                Authorization: `Bearer ${token}`,
+            },
+            onUploadProgress: (progressEvent) => {
+            },
+        });
+        loading.value = false
+    } catch (e){
+        loading.value = false
+        response = {
+            'id': 6,
+            'size': 5,
+            'src': 'https://videocrat.com/storage/instagram/reels/3782546227448034289.mp4',
+            'title': 'video_6'
+        }
+    }
 }
 
 </script>
